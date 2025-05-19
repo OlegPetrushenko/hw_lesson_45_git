81438eed478b5bd2ea96db5f801bd771d7f84475
OlegPetrushenko <algoritm.rts@gmail.com>
2025-05-19 20:02:28 +0200

    Add Rectangle class

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(5.0, 3.0);
+
+        double area = rect.getArea();
+        double perimeter = rect.getPerimeter();
+
+        System.out.println("Площадь прямоугольника: " + area);
+        System.out.println("Периметр прямоугольника: " + perimeter);
+    }
+}
