d6b39fed8fd81db7867887f5c6d5215be5029ea5
OlegPetrushenko <algoritm.rts@gmail.com>
2025-05-19 20:12:58 +0200

    Variable values have been changed

--- src/main/java/task_01/Main.java
+++ src/main/java/task_01/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        int a = 4;
-        int b = 7;
+        int a = 45;
+        int b = 45;
         int result = calculator.add(a, b);
 
         System.out.println("Сумма: " + result);
